5383bd124f5337b6ca4b81226f8af7f7a49d4f1f
Serge <sanichace@gmail.com>
2020-11-26 22:22:18 +0200

    Добавьте файлы проекта.

---
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        => RedirectToPage("Index");
+    }
+}
