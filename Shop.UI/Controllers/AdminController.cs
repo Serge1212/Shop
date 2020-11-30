b57466dd77d382b6c48e743c055b34ebc688b22a
Serge <sanichace@gmail.com>
2020-11-30 21:30:11 +0200

    implemented Stock functionality in AdminController

--- Shop.UI/Controllers/AdminController.cs
+++ Shop.UI/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Application.ProductsAdmin;
+using Shop.Application.StockAdmin;
 using Shop.Database;
 using System.Threading.Tasks;
 
@@ -13,15 +14,35 @@ namespace Shop.UI.Controllers
         {
             _context = context;
         }
+        #region Products Methods
         [HttpGet("products")]
         public IActionResult GetProducts() => Ok(new GetProducts(_context).Do());
+
         [HttpGet("products/{id}")]
         public IActionResult GetProduct(int id) => Ok(new GetProduct(_context).Do(id));
+
         [HttpPost("products")]
         public async Task<IActionResult> CreateProduct([FromBody]CreateProduct.Request request) => Ok((await new CreateProduct(_context).Do(request)));
+
         [HttpDelete("products/{id}")]
         public async Task<IActionResult> DeleteProduct(int id) => Ok((await new DeleteProduct(_context).Do(id)));
+
         [HttpPut("products")]
         public async Task<IActionResult> UpdateProduct([FromBody]UpdateProduct.Request request) => Ok((await new UpdateProduct(_context).Do(request)));
+        #endregion
+
+        #region Stock Methods
+        [HttpGet("stocks")]
+        public IActionResult GetStock() => Ok(new GetStock(_context).Do());
+
+        [HttpPost("stocks")]
+        public async Task<IActionResult> CreateStock([FromBody]CreateStock.Request request) => Ok((await new CreateStock(_context).Do(request)));
+
+        [HttpDelete("stocks/{id}")]
+        public async Task<IActionResult> DeleteStock(int id) => Ok((await new DeleteStock(_context).Do(id)));
+
+        [HttpPut("stocks")]
+        public async Task<IActionResult> UpdateStock([FromBody]UpdateStock.Request request) => Ok((await new UpdateStock(_context).Do(request)));
+        #endregion
     }
 }
